84b7a04dee8047918abd94be84622f282fd8d2ad
tinaxd <31384595+tinaxd@users.noreply.github.com>
2024-04-11 00:09:42 +0900

    first

--- barcoder/barcoder.swift
+++ barcoder/barcoder.swift
@@ -2,9 +2,59 @@
 //  barcoder.swift
 //  barcoder
 //
-//  Created by Chinamu Kawano on 2024/04/10.
+//  Created by tinaxd on 2024/04/10.
 //
 
-class barcoder {
+public enum EncodeError: Error {
+    case UnencodableCharacter
+}
 
+public enum BarcoderError: Error {
+    case InvalidConfiguration
+}
+
+open class Barcoder {
+    var use_code_b: Bool
+    
+    public init(use_code_b: Bool) throws {
+        self.use_code_b = use_code_b
+        
+        if (!self.use_code_b) {
+            throw BarcoderError.InvalidConfiguration
+        }
+    }
+    
+    open func encode_string(value: String) -> Result<Array<UInt8>, EncodeError> {
+        return self.encode_b(value: value);
+    }
+    
+    private func encode_b(value: String) -> Result<Array<UInt8>, EncodeError> {
+        var encoded = Array<UInt8>();
+        
+        // start code
+        encoded += code128_patterns[code128_IDX_START_B];
+        
+        // encode body
+        var checksum = code128_IDX_START_B;
+        for (i, ch) in value.enumerated() {
+            let idx = code128_B[ch];
+            if let idx = idx {
+                encoded += code128_patterns[idx];
+                
+                // update checksum
+                checksum += (i+1) * idx;
+                checksum %= 103;
+            } else {
+                return .failure(.UnencodableCharacter)
+            }
+        }
+        
+        // append checksum
+        encoded += code128_patterns[checksum];
+        
+        // stop code
+        encoded += code128_patterns[code128_IDX_STOPCODE];
+        
+        return .success(encoded);
+    }
 }
---
+++ barcoderTests/barcoderTests.swift
@@ -0,0 +1,42 @@
+//
+//  barcoderTests.swift
+//  barcoderTests
+//
+//  Created by tinaxd on 2024/04/10.
+//
+
+import XCTest
+import barcoder
+
+final class barcoderTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        let encoder = try Barcoder(use_code_b: true)
+        XCTAssertEqual(try encoder.encode_string(value: "tinax2").get(), [1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1])
+        XCTAssertEqual(try encoder.encode_string(value: "1234abcD!").get(), [1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1])
+        XCTAssertEqual(try encoder.encode_string(value: "very looooong !!! 2024").get(), [1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1])
+        
+        
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
